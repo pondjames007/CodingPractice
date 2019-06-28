dfc601578edc843150ac22d38751f5f75fd02887
pondjames007 <jameshuang@nyu.edu>
2019-06-28 14:07:27 -0400

    update note

--- String/792_NumberOfMatchingSubsequences.py
+++ String/792_NumberOfMatchingSubsequences.py
@@ -1,3 +1,5 @@
 # TIPS:
 # * subsequence: letter order matters
 # * bisect: use binary search in a SORTED list
+# * bisect.bisect_left(list, x): return the index that can insert x in list, if x is in list, it will return the index at left
+# * bisect.insort_left(list, x): insert x into the ordered list
